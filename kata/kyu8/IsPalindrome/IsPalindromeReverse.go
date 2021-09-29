615718151cdeb92b61c3e26863368dc28ca73d1c
Sergey Kudashev <kudashevs@gmail.com>
2021-09-29 23:12:03 +0300

    Refactor "Is it a palindrome" rename reverse function.

--- kata/kyu8/IsPalindrome/IsPalindromeReverse.go
+++ kata/kyu8/IsPalindrome/IsPalindromeReverse.go
@@ -6,12 +6,12 @@ import (
 
 func IsPalindromeReverse(str string) bool {
 	str = strings.ToLower(str)
-	rev := reverseString(str)
+	rev := reverse(str)
 
 	return str == rev
 }
 
-func reverseString(str string) (res string) {
+func reverse(str string) (res string) {
 	for _, v := range str {
 		res = string(v) + res
 	}
