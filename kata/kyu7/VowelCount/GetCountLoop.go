b6a643e0756cc87616283a8f5f07a12f7c9f0a6e
Sergey Kudashev <kudashevs@gmail.com>
2021-10-01 09:14:49 +0300

    Refactor "Vowel Count" remove redundant variable in loop solution.

--- kata/kyu7/VowelCount/GetCountLoop.go
+++ kata/kyu7/VowelCount/GetCountLoop.go
@@ -3,14 +3,13 @@ package vowelcount
 import "strings"
 
 func GetCountLoop(str string) (count int) {
-	vowelsCount := 0
 	vowels := `aeiou`
 
 	for _, char := range str {
 		if strings.ContainsRune(vowels, char) {
-			vowelsCount++
+			count++
 		}
 	}
 
-	return vowelsCount
+	return
 }
