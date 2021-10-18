1a8ecc257abca017f34d52b3ce886aab4c14cd7b
Sergey Kudashev <kudashevs@gmail.com>
2021-10-18 23:01:47 +0300

    Fix "Sum of positive" uncomment mistakenly commented tests.

--- test/kyu8/SumOfPositive/PositiveSum_test.go
+++ test/kyu8/SumOfPositive/PositiveSum_test.go
@@ -10,9 +10,9 @@ import (
 var _ = Describe("PositiveSum", func() {
 	It("should test that the solution returns the correct value", func() {
 		Expect(PositiveSum([]int{1, 2, 3, 4, 5})).To(Equal(15))
-		// Expect(PositiveSum([]int{1, -2, 3, 4, 5})).To(Equal(13))
-		// Expect(PositiveSum([]int{})).To(Equal(0))
-		// Expect(PositiveSum([]int{-1, -2, -3, -4, -5})).To(Equal(0))
-		// Expect(PositiveSum([]int{-1, 2, 3, 4, -5})).To(Equal(9))
+		Expect(PositiveSum([]int{1, -2, 3, 4, 5})).To(Equal(13))
+		Expect(PositiveSum([]int{})).To(Equal(0))
+		Expect(PositiveSum([]int{-1, -2, -3, -4, -5})).To(Equal(0))
+		Expect(PositiveSum([]int{-1, 2, 3, 4, -5})).To(Equal(9))
 	})
 })
